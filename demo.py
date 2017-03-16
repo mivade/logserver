f369c80d42b0223c736b5b82099d657e7ee710c6
Michael V. DePalatis <mike@depalatis.net>
2017-03-16 13:36:41 -0400

    Add demo

---
+++ demo.py
@@ -0,0 +1,20 @@
+import time
+import random
+import logging
+from multiprocessing import Process
+import logserver
+from logserver.handlers import SQLiteHandler
+
+
+logger = logserver.create_logger("demo")
+
+p = Process(target=logserver.run_server, args=[[SQLiteHandler("logs.sqlite")]])
+p.start()
+
+for _ in range(10):
+    delay = random.randint(0, 10)
+    logger.warning("Sleeping for %d seconds...", delay)
+    time.sleep(delay)
+    logger.info("Awoke!")
+
+p.terminate()
