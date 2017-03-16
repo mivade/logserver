5372aa76e9ba32be33789becd13705c0827889b0
Michael V. DePalatis <mike@depalatis.net>
2017-03-15 21:12:00 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open("README.rst", 'r') as f:
+    readme = f.read()
+
+
+setup(
+    name="logserver",
+    version="0.1.0",
+    author="Michael V. DePalatis",
+    author_email="mike@depalatis.net",
+    description="Reusable, dependency-free log server backed by SQLite",
+    long_description=readme,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
+    ]
+)
